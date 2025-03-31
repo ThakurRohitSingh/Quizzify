b61f1a5a2efa4a6bb00736bb24a6afa4670e6e4d
Rohit Singh Thakur <thakurrohitsingh04@gmail.com>
2025-03-31 19:25:38 +0530

    add some logic

--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -1,7 +1,20 @@
 const Question = (props) => {
     let normalButtonElements = []; //For Normal Buttons at start of Quiz
     let answerButtonElements = []; //For AnswerButtons to show correct answers
-   
+    if (props.isChecked === false) {
+      normalButtonElements = props.options.map((option) => {
+        return (
+          <button
+            id={props.selectedAnswer === option.choice ? "selected" : ""}
+            key={option.id}
+            onClick={() => props.handleButtonClick(option.choice, props.id)}
+            className="normal-button"
+          >
+            {option.choice}
+          </button>
+        );
+      });
+    } else {}
     return (
       <div className="question">
         <h1 className="question-title">Whole Question is there</h1>

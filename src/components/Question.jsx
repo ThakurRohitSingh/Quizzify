be26e8ddc4d82850702fd7db41280460139e3601
Rohit Singh Thakur <thakurrohitsingh04@gmail.com>
2025-03-31 18:52:14 +0530

    Design for Questions

---
+++ src/components/Question.jsx
@@ -0,0 +1,15 @@
+const Question = (props) => {
+    let normalButtonElements = []; //For Normal Buttons at start of Quiz
+    let answerButtonElements = []; //For AnswerButtons to show correct answers
+   
+    return (
+      <div className="question">
+        <h1 className="question-title">Whole Question is there</h1>
+        there i need to check for the option
+        <hr className="question-divider" />
+      </div>
+    );
+
+}
+
+export default Question

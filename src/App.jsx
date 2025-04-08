c90e172f9ef49a52b794b8bac2486f66959e3b5b
Rohit Singh Thakur <thakurrohitsingh04@gmail.com>
2025-04-08 23:32:35 +0530

    Wrap all the component inside contextapi

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,30 @@
-import { useState } from "react"
-import "./App.css"
-import LandingPage from "./components/LandingPage"
-import Quiz from "./components/Quiz"
-
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import LandingPage from './components/LandingPage';
+import Login from './components/Login';
+import SignUp from './components/Sign';
+import ResetPassword from './components/ResetPassword';
+import SelectTopics from './components/SelectTopics';
+import Quiz from './components/Quiz';
+import { QuizProvider } from './components/QuizContext';
 
 const App = () => {
-  const [isQuizStarted,setisQuizStarted] = useState(false)
-
-  function handleClick(){
-    setisQuizStarted((prev) => !prev)
-  }
   return (
    <>
-   {
-    isQuizStarted ? <Quiz/> : <LandingPage isQuizStarted={handleClick}/>
-   }
+    <QuizProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/forgotpassword" element={<ResetPassword />} />
+          <Route path="/select" element={<SelectTopics />} />
+          <Route path="/quiz" element={<Quiz />} />
+        </Routes>
+      </BrowserRouter>
+    </QuizProvider>
    </>
-  )
-}
+  );
+};
 
 export default App;

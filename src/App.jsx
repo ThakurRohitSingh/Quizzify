eb0522b866b49a697f0f65cf89b31a5ded96be67
Rohit Singh Thakur <thakurrohitsingh04@gmail.com>
2025-04-13 22:44:43 +0530

    Add forgot password routing

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,10 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import LandingPage from './components/LandingPage';
 import Login from './components/Login';
 import SignUp from './components/Sign';
-import ResetPassword from './components/ResetPassword';
 import SelectTopics from './components/SelectTopics';
 import Quiz from './components/Quiz';
 import { QuizProvider } from './components/QuizContext';
+import FogotPassword from './components/ForgotPassword';
 
 const App = () => {
   return (
@@ -17,7 +17,7 @@ const App = () => {
           <Route path="/" element={<LandingPage />} />
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<SignUp />} />
-          <Route path="/forgotpassword" element={<ResetPassword />} />
+          <Route path="/forgotpassword" element={<FogotPassword/>} />
           <Route path="/select" element={<SelectTopics />} />
           <Route path="/quiz" element={<Quiz />} />
         </Routes>

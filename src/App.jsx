2a8dc01d52a2bf093a3367fa12cccba0610c9765
Rohit Singh Thakur <thakurrohitsingh04@gmail.com>
2025-04-14 20:19:37 +0530

    refactor: relocate auth components

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import LandingPage from './components/LandingPage';
-import Login from './components/Login';
-import SignUp from './components/Sign';
 import SelectTopics from './components/SelectTopics';
 import Quiz from './components/Quiz';
 import { QuizProvider } from './components/QuizContext';
 import FogotPassword from './components/ForgotPassword';
+import Login from './components/Auth/Login';
+import SignUp from './components/Auth/Sign';
 
 const App = () => {
   return (
@@ -15,8 +15,8 @@ const App = () => {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<LandingPage />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<SignUp />} />
+          <Route path="/login" element={<Login/>} />
+          <Route path="/signup" element={<SignUp/>} />
           <Route path="/forgotpassword" element={<FogotPassword/>} />
           <Route path="/select" element={<SelectTopics />} />
           <Route path="/quiz" element={<Quiz />} />

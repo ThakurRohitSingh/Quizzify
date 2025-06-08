62dbe5388c5062e60c69b70914a2d0d0c26bee8d
Rohit Singh Thakur <thakurrohitsingh04@gmail.com>
2025-06-08 23:49:04 +0530

    Add Protected Routes

--- src/App.jsx
+++ src/App.jsx
@@ -7,20 +7,29 @@ import { QuizProvider } from './components/QuizContext';
 import FogotPassword from './components/ForgotPassword';
 import Login from './components/Auth/Login';
 import SignUp from './components/Auth/Sign';
+import AuthProvider from './components/Auth/AuthProvider';
+import ProtectedRoutes from './components/Auth/ProtectedRoutes';
 
 const App = () => {
   return (
    <>
     <QuizProvider>
       <BrowserRouter>
-        <Routes>
+       <AuthProvider>
+       <Routes>
           <Route path="/" element={<LandingPage />} />
           <Route path="/login" element={<Login/>} />
           <Route path="/signup" element={<SignUp/>} />
           <Route path="/forgotpassword" element={<FogotPassword/>} />
-          <Route path="/select" element={<SelectTopics />} />
-          <Route path="/quiz" element={<Quiz />} />
+          {/* reset Password you need to add */}
+          <Route path="/select" element={<ProtectedRoutes>
+            <SelectTopics />
+          </ProtectedRoutes>} />
+          <Route path="/quiz" element={<ProtectedRoutes>
+            <Quiz />
+          </ProtectedRoutes>} />
         </Routes>
+       </AuthProvider>
       </BrowserRouter>
     </QuizProvider>
    </>
